585c598b1267dd51f0538b3d4af267d49d355259
AmanNegi <akuro787898@gmail.com>
2021-02-02 18:20:08 +0530

    Day 1: Final Commit (Fixed Minor errors):

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:day_1/welcome_page.dart';
+import 'package:Flutter30Days/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
